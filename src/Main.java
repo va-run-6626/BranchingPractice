d282879f412f4c71c470830c644631fd655f0911
va-run-6626 <vbhutada143@gmail.com>
2025-04-13 21:15:30 +0530

    for loop added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 89; i++){
+            System.out.println(i);
+        }
+    }
+}
